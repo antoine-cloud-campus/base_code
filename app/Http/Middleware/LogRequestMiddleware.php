2323bd4107bc6d4ecf18dc4eea172a3c193f5e6d
Nubunduram <ceantoine@hotmail.fr>
2026-01-14 12:05:30 +0100

    2

---
+++ app/Http/Middleware/LogRequestMiddleware.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\Log;
+
+class LogRequestMiddleware
+{
+    public function handle(Request $request, Closure $next): Response
+    {
+        $userAgent=$request->header('User-Agent');
+        
+        Log::info('Request User-Agent', [
+            'user-agent' => $userAgent,
+            'ip' => $request->ip(),
+            'path' => $request->path(),
+            'method' => $request->method(),
+            'user_id' => optional($request->user())->id,
+        ]);
+        
+        return $next($request);
+    }
+}
